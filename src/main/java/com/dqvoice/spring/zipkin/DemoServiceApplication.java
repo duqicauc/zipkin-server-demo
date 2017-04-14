e4094606488a8a443e6119d35a9a8b39c365c415
duqi <duqi@youzan.com>
2017-04-14 16:31:58 +0800

    first commit

---
+++ src/main/java/com/dqvoice/spring/zipkin/DemoServiceApplication.java
@@ -0,0 +1,20 @@
+package com.dqvoice.spring.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class DemoServiceApplication {
+
+	@GetMapping("/hi")
+	public String hi() {
+		return "Hello World";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoServiceApplication.class, args);
+	}
+}
